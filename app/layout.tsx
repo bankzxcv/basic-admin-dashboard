bc8f909b65cd22c7d4036f1209d4e4a3b541c792
Jirayu <jirayu.saeng@gmail.com>
2024-07-19 02:01:59 -0700

    updated dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import type { Metadata } from "next"
 import Navbar from "../components/navbar"
 import Sidebar from "../components/sidebar"
+import { ThemeProvider } from "@/components/providers/ThemeProvider"
 import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -21,14 +22,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] w-[300px]">
-            <Sidebar />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+          storageKey="dashboard-theme"
+        >
+          <Navbar />
+          <div className="flex">
+            <div className="hidden md:block h-[100vh] w-[300px]">
+              <Sidebar />
+            </div>
+            <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
           </div>
-          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
-        </div>
-        <Toaster />
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   )
